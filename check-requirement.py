a0df237c5d4cf19896bdf6855fbdc1e23056db3e
marshanabilah <marsha.nabilah@gmail.com>
2025-03-13 03:45:25 +0000

    change 7b to 3b

--- check-requirement.py
+++ check-requirement.py
@@ -2,9 +2,11 @@ from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
 
 model = AutoModelForCausalLM.from_pretrained('aisingapore/sea-lion-3b', trust_remote_code=True).to('cuda')
-print(model)
+print(model.config)
 
-state_dict = model.state_dict()
-for key in state_dict.keys():
-    print(key)
+model1 = AutoModelForCausalLM.from_pretrained('openai-community/gpt2-xl', trust_remote_code=True).to('cuda')
+print(model1.config)
 
+# state_dict = model.state_dict()
+# for key in state_dict.keys():
+#     print(key)
